9175afe8f86fabfb396b6017f91546835801ba61
sudas <das.sukanta2@gmail.com>
2026-01-14 16:08:02 +0100

    backup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
 import json
+import time
+
+start = time.time()
+
 from llm_sdk import Small_LLM_Model
 
-small_llm = Small_LLM_Model()
-file_path = small_llm.get_path_to_vocabulary_json()
-print(file_path)
-print("Testing the uv venv")
+llm = Small_LLM_Model(device="cpu")
+# file_path = small_llm.get_path_to_vocabulary_json()
+# txt = "What is the sum of 2 and 3?"
+# tokens = llm._encode(txt)
+# token_list = [token for token in tokens[0]]
+# print(token_list)
+# logits = llm.get_logits_from_input_ids(token_list)
+# print(logits)
+# decoded_logits = llm._decode()
+
+tokens = ["What", "Ġis", "Ġthe", "Ġsum", "Ġof",
+          "Ġ2", "Ġand", "Ġ3", "?"]
+
+path = "/home/sudas/.cache/huggingface/hub/" \
+       "models--Qwen--Qwen3-0.6B/snapshots/" \
+       "c1899de289a04d12100db370d81485cdf75e47ca/vocab.json"
+with open(path, "r") as fl:
+    data = json.loads(fl.read())
+
+token_list = [data.get(token) for token in tokens if data.get(token) is not None]
+print(token_list)
+
+logits = llm.get_logits_from_input_ids(token_list)
+print(len(logits))
+# print(data)
+
+end = time.time()
+print(f"Testing the LLM, Time: {(end - start):.3f}")
